13fd9634b10db1832b6859f4fcc43bc7af62e0e8
Jyotheeswar114 <jyotheeswarganne60@gmail.com>
2023-02-23 10:03:31 +0530

    added constants

--- experiment/simulation/src/App.js
+++ experiment/simulation/src/App.js
@@ -3,10 +3,27 @@ import Pool from './Pool.js';
 
 import React, { Component } from 'react'
 
+import * as Constants from './constants';
+
+
 export default class App extends Component {
+  
   render() {
     return (
-      <Pool name="Hello Akshay" processes={[1, 3, 4, 9, 10]} />
+      <div class="main">
+        <div id="events">
+
+        </div>
+        <div id="pools">
+          <Pool name="Ready Pool" processes={[1, 3, 4, 9, 10]} />
+          <Pool name="CPU Pool" processes={[1, 3, 4, 9, 10]} />
+          <Pool name="IO Pool" processes={[1, 3, 4, 9, 10]} />
+          <Pool name="Completed Pool" processes={[1, 3, 4, 9, 10]} />
+        </div>
+        <div id="log">
+
+        </div>
+      </div>
     )
   }
 }
